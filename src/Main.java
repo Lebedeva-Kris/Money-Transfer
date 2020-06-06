e99e76a035e6594f47041b7a3d352d1f50753be7
Kristina <kris2015.lebedeva@yandex.ru>
2020-06-06 21:22:08 +0300

    java added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int current = 2_000_000_000;
-        int transfer = 500_000_000;
+        int transfer = 1_000_000_000;
         int total = current + transfer;
         System.out.println(total);
     }
 }
+
